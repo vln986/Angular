0973d3c742c02013d5af23b80fa205761e50c896
vln986 <laxminarayanaveggalam@gmail.com>
2025-01-25 19:50:40 +0530

    readme and types

---
+++ readme.txt
@@ -0,0 +1,12 @@
+******* Angular *******
+1.Installation 
+    npm install -g typescript
+
+2.To check version 
+    tsc --version
+
+3. Compile 
+    tsc <fileName>.ts
+
+4. run
+    node <fileName>.js 
---
+++ types.ts
@@ -0,0 +1,13 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[]=[1,2,3];
+let f: any[] = [1, true, 'a', false];
+
+const colorRed = 0;
+const colorGreen = 1;
+const colorBlue = 2;
+
+enum color {Red = 0,Green = 1,Blue = 2,Purple = 3};
+let backgroundColor = color.Purple;
